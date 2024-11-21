7301e6e65cf1115881deca582dd0583e14c5afc3
lj <71525498+ekovanda@users.noreply.github.com>
2024-11-21 22:05:36 +0100

    Simplified display. Added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,17 +14,17 @@ def main():
     if "nadeo_jwt_token" not in st.session_state:
         st.session_state["nadeo_jwt_token"] = get_nadeo_jwt_token(st.session_state["ubisoft_ticket"])
 
+    # App Body
+    st.title("Trackmania Tracker 🏆🏎")
     selected_track = st.selectbox("Track Number", options=list(range(1,26)))
 
     if selected_track:
         pbs_raw: Dict = get_club_track_pbs(CLUBS[0], TRACKS[selected_track - 1], jwt_token=st.session_state["nadeo_jwt_token"]["accessToken"])
         pbs: Dict = postprocess_club_track_pbs(pbs_raw)
-
-
-    # App Body
-    st.title("Trackmania Tracker")
-    st.write(pbs)
-
+        st.header(pbs["track"].name)
+        for idx, player in enumerate(pbs["players"]):
+            icons = ["🏆", "🥈", "🥉"]
+            st.write(f"### {icons[idx]} {player["pb"]}: \t {player["player"].alias or player["player"].name}")
 
 if __name__ == "__main__":
     main()
