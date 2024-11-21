d83307ad7f0795f851826ed78415e30e0f63c841
lj <elliot.kovanda@gmail.com>
2024-11-21 21:31:56 +0100

    Add track selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from typing import Dict
 
 from authentication import get_ubisoft_authentication_ticket, get_nadeo_jwt_token
-from live_services import get_club_map_pbs
-from tm_lookups import CLUBS, MAPS, PLAYERS
+from live_services import get_club_track_pbs, postprocess_club_track_pbs
+from tm_lookups import CLUBS, TRACKS, PLAYERS
 
 
 def main():
@@ -14,8 +14,12 @@ def main():
     if "nadeo_jwt_token" not in st.session_state:
         st.session_state["nadeo_jwt_token"] = get_nadeo_jwt_token(st.session_state["ubisoft_ticket"])
 
+    selected_track = st.selectbox("Track Number", options=list(range(1,26)))
 
-    pbs: Dict = get_club_map_pbs(CLUBS[0], MAPS[0], jwt_token=st.session_state["nadeo_jwt_token"]["accessToken"])
+    if selected_track:
+        pbs_raw: Dict = get_club_track_pbs(CLUBS[0], TRACKS[selected_track - 1], jwt_token=st.session_state["nadeo_jwt_token"]["accessToken"])
+        pbs: Dict = postprocess_club_track_pbs(pbs_raw)
+
 
     # App Body
     st.title("Trackmania Tracker")
--- tm_lookups.py
+++ tm_lookups.py
@@ -12,7 +12,7 @@ class Club:
     owner_alias: Optional[str]
 
 @dataclass
-class Map:
+class Track:
     name: str
     uid: str
 
@@ -24,7 +24,34 @@ class Player:
 
 CLUBS: List[Club] = [Club("85915", "7e468ff6-4558-43ce-ad23-591dd86291c0", "Elliot")] 
 
-MAPS: List[Map] = [Map("Fall-12", "MXCtvB4zmW3iliJ48Jti6luY9Zi")]
+# Manually obtained from: https://trackmania.io/#/campaigns/0/77963 
+TRACKS: List[Track] = [
+    Track("Fall-01", "rw7jr8WlTrYor0vN0A0PiKzgg78"),
+    Track("Fall-02", "tl_RqArUrUQ9KQDe0U5fskzdrpj"),
+    Track("Fall-03", "6h3gOaF9HJBCnQbxtBopC_IMs10"),
+    Track("Fall-04", "ecpLvJZCuyoAJj1LhfZZwhoHfSa"),
+    Track("Fall-05", "d83GbrV9SN7kfY3ftaK8FRVB44b"),
+    Track("Fall-06", "trDIbaxC_9IVdg6yen9q7zISZbk"),
+    Track("Fall-07", "q5L8T_fy0w5MkZfJO1gQ_my87bj"),
+    Track("Fall-08", "2msqSkfJP683MmDGhfpkJM4q7Sk"),
+    Track("Fall-09", "ti8ot1biy4yCUdi7lPmGqFE6aRj"),
+    Track("Fall-10", "BKwVaqdgCe4PCWQwluvi99iLe5b"),
+    Track("Fall-11", "3sDimlZCd0QPmmRiGASA1Pbtv05"),
+    Track("Fall-12", "MXCtvB4zmW3iliJ48Jti6luY9Zi"),
+    Track("Fall-13", "glysLJbIhBkhDFRB7zhvhatgIqa"),
+    Track("Fall-14", "SnLRynzusvgCyqlu9X0Ux_NkcMd"),
+    Track("Fall-15", "WGEcRcfn0NGxxrodXzshYdwzfIg"),
+    Track("Fall-16", "5BretnXASvk2RoKa2WqFOcnW9x7"),
+    Track("Fall-17", "yCaeXB3t3c8_SrsIBefjDws5hqg"),
+    Track("Fall-18", "DrJSswMXezYuYTS8bEjjQmdWDVa"),
+    Track("Fall-19", "JndUMBgeR4vQ2zadb164PFOBS9"),
+    Track("Fall-20", "uTRn1j2ZvRr02krtvKJQJqEGwH7"),
+    Track("Fall-21", "p5tWnXUzwNxyxgsg7m8ZoaUbqAa"),
+    Track("Fall-22", "S927zT5HCIybPpRgh5RY79Zcb5h"),
+    Track("Fall-23", "WxCghen5pI7lbDKigtekKq0Uut3"),
+    Track("Fall-24", "9fEY2mqkUY50nhtj9c0NXWax5xk"),
+    Track("Fall-25", "End5ikYJa8pgN8RI347E6l4i7lg"),
+    ]
 
 PLAYERS: List[Player] = [
     Player("EljayKay", "7e468ff6-4558-43ce-ad23-591dd86291c0", "Elliot"),
