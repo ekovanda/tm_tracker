35f10fdd06693db820914fa473f6c99d38ad098e
lj <71525498+ekovanda@users.noreply.github.com>
2026-08-08 11:55:54 +0200

    refactor(app) remove obsolete views

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,6 @@ from authentication import (
     get_nadeo_service_token,
 )
 from streamlit_bingo_page import bingo_page
-from streamlit_player_focus_page import player_focus_page
-from streamlit_track_focus_page import track_focus_page
 
 
 def main():
@@ -19,20 +17,8 @@ def main():
             st.error(str(error))
             st.stop()
 
-    # App Body
     st.title("Trackmania Tracker 🏆🏎")
-
-    with st.sidebar:
-        add_radio = st.radio(
-            label="Choose a Page", options=("Bingo", "Player View", "Track View")
-        )
-
-    if add_radio == "Player View":
-        player_focus_page()
-    elif add_radio == "Track View":
-        track_focus_page()
-    else:
-        bingo_page()
+    bingo_page()
 
 
 if __name__ == "__main__":
